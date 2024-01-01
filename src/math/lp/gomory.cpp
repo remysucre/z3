63e67cc5e54dd3ff32b1f1abedfafea4ebe02d9c
Lev Nachmanson <levnach@hotmail.com>
2024-01-01 09:46:23 -1000

    simplify gomory cut
    Signed-off-by: Lev Nachmanson <levnach@hotmail.com>

--- src/math/lp/gomory.cpp
+++ src/math/lp/gomory.cpp
@@ -22,7 +22,6 @@
 #include "math/lp/lar_solver.h"
 #include "math/lp/lp_utils.h"
 
-#define SMALL_CUTS 1
 namespace lp {
 
 struct create_cut {
@@ -36,9 +35,7 @@ struct create_cut {
     mpq                   m_one_minus_f;
     mpq                   m_fj;
     mpq                   m_one_minus_fj;
-#if SMALL_CUTS
     mpq                   m_abs_max, m_big_number;
-#endif
     int                   m_polarity;
     bool                  m_found_big;
     u_dependency*         m_dep;
@@ -54,6 +51,11 @@ struct create_cut {
     u_dependency* column_lower_bound_constraint(unsigned j) const { return lia.column_lower_bound_constraint(j); }
     u_dependency* column_upper_bound_constraint(unsigned j) const { return lia.column_upper_bound_constraint(j); }
     bool column_is_fixed(unsigned j) const { return lia.lra.column_is_fixed(j); }
+
+    bool number_is_too_big(mpq const& a) const {
+        return numerator(a) > m_big_number;
+    }
+
     void push_explanation(u_dependency* d) {
         for (auto ci : lia.lra.flatten(d))
             m_ex->push_back(ci);
@@ -84,12 +86,7 @@ struct create_cut {
         }        
         m_t.add_monomial(new_a, j);
         TRACE("gomory_cut_detail", tout << "new_a = " << new_a << ", k = " << m_k << "\n";);
-#if SMALL_CUTS
-        // if (numerator(new_a).is_big()) throw found_big(); 
-        if (numerator(new_a) > m_big_number)
-            m_found_big = true;
-#endif
-    }
+		if (number_is_too_big(new_a)) m_found_big = true;
 
     void set_polarity(int p) {
         if (m_polarity == 2) return;
@@ -134,11 +131,7 @@ struct create_cut {
         TRACE("gomory_cut_detail_real", tout << "add " << new_a << "*v" << j << ", k: " << m_k << "\n";
               tout << "m_t =  "; lia.lra.print_term(m_t, tout) << "\nk: " << m_k << "\n";);
         
-#if SMALL_CUTS
-        // if (numerator(new_a).is_big()) throw found_big(); 
-        if (numerator(new_a) > m_big_number)
-            m_found_big = true;
-#endif
+        if (number_is_too_big(new_a)) m_found_big = true;
     }
 
     lia_move report_conflict_from_gomory_cut() {
@@ -266,15 +259,11 @@ public:
         lp_assert(m_f.is_pos() && (get_value(m_inf_col).x - m_f).is_int());  
 
         bool some_int_columns = false;
-#if SMALL_CUTS
         m_abs_max = 0;
-        for (const auto & p : m_row) {
-            mpq t = abs(ceil(p.coeff()));
-            if (t > m_abs_max)
-                m_abs_max = t;
-        }
+        for (const auto & p : m_row)
+            m_abs_max = std::max(m_abs_max, abs(p.coeff()));
+
         m_big_number = m_abs_max.expt(2);
-#endif
         for (const auto & p : m_row) {
             unsigned j = p.var();
             if (j == m_inf_col) continue;
@@ -305,23 +294,16 @@ public:
                         set_polarity(1);
                 }
             }
-
-            if (m_found_big) {
-                return lia_move::undef;
-            }
         }
 
         if (m_t.is_empty()) {
-            return report_conflict_from_gomory_cut();
+            return report_conflict_from_gomory_cut(); // todo: actually, report the conflict!
         }
-        TRACE("gomory_cut", print_linear_combination_of_column_indices_only(m_t.coeffs_as_vector(), tout << "gomory cut: "); tout << " >= " << m_k << std::endl;);
-        if (lia.lra.settings().m_gomory_simplify && some_int_columns)
-            simplify_inequality();
-        
         m_dep = nullptr;
         for (auto c : *m_ex) 
          	m_dep = lia.lra.join_deps(lia.lra.dep_manager().mk_leaf(c.ci()), m_dep);
 
+        TRACE("gomory_cut", print_linear_combination_of_column_indices_only(m_t.coeffs_as_vector(), tout << "gomory cut: "); tout << " >= " << m_k << std::endl;);
         TRACE("gomory_cut_detail", dump_cut_and_constraints_as_smt_lemma(tout);
               lia.lra.display(tout));
         SASSERT(lia.current_solution_is_inf_on_cut());
@@ -329,99 +311,6 @@ public:
         lia.settings().stats().m_gomory_cuts++;
         return lia_move::cut;
     }
-
-    // TODO: use this also for HNF cuts?
-    mpq                 m_lcm_den = { mpq(1) };
-    
-    void simplify_inequality() {
-
-        auto divd = [](mpq& r, mpq const& d) {
-            r /= d;
-            if (!r.is_int())
-                r = ceil(r);
-        };
-        SASSERT(!lia.m_upper);
-        lp_assert(!m_t.is_empty());
-        // k = 1 + sum of m_t at bounds
-        lar_term t = lia.lra.unfold_nested_subterms(m_t);
-        auto pol = t.coeffs_as_vector();
-        m_t.clear();
-        if (pol.size() == 1 && is_int(pol[0].second)) {
-            TRACE("gomory_cut_detail", tout << "pol.size() is 1" << std::endl;);
-            auto const& [a, v] = pol[0];
-            lp_assert(is_int(v));
-            if (a.is_pos()) { // we have av >= k
-                divd(m_k, a);
-                m_t.add_monomial(mpq(1), v);
-            }
-            else {
-                // av >= k
-                // a/-a*v >= k / - a
-                // -v >= k / - a
-                // -v >= ceil(k / -a)
-                divd(m_k, -a);
-                m_t.add_monomial(-mpq(1), v);
-            }
-        }
-        else {
-            m_lcm_den = denominator(m_k);
-            for (auto const& [c, v] : pol)
-                m_lcm_den = lcm(m_lcm_den, denominator(c));
-            lp_assert(m_lcm_den.is_pos());
-            bool int_row = all_of(pol, [&](auto const& kv) { return is_int(kv.second); });
-            TRACE("gomory_cut_detail", tout << "pol.size() > 1 den: " << m_lcm_den << std::endl;);
-                
-            if (!m_lcm_den.is_one()) {
-                // normalize coefficients of integer parameters to be integers.
-                for (auto & [c,v]: pol) {
-                    c *= m_lcm_den;
-                    SASSERT(!is_int(v) || c.is_int());
-                }
-                m_k *= m_lcm_den;
-            }
-            // ax + by >= k
-            // b > 0, c1 <= y <= c2
-            // ax + b*c2 >= ax + by >= k
-            // => 
-            // ax >= k - by >= k - b*c1
-            // b < 0
-            // ax + b*c1 >= ax + by >= k
-            // 
-            unsigned j = 0, i = 0;
-            for (auto & [c, v] : pol) {
-                if (lia.is_fixed(v)) {
-                    push_explanation(column_lower_bound_constraint(v));
-                    push_explanation(column_upper_bound_constraint(v));
-                    m_k -= c * lower_bound(v).x;
-                }
-                else
-                    pol[j++] = pol[i];
-                ++i;
-            }
-            pol.shrink(j);
-
-            // gcd reduction is loss-less:
-            mpq g(1);
-            for (const auto & [c, v] : pol)
-                g = gcd(g, c);
-            if (!int_row)
-                g = gcd(g, m_k);
-
-            if (g != 1) {
-                for (auto & [c, v] : pol)
-                    c /= g;
-                divd(m_k, g);
-            }
-                
-            for (const auto & [c, v]: pol) 
-                m_t.add_monomial(c, v);
-            VERIFY(m_t.size() > 0);
-        }
-
-        TRACE("gomory_cut_detail", tout << "k = " << m_k << std::endl;);
-        lp_assert(m_k.is_int());
-    }
-
 
     create_cut(lar_term & t, mpq & k, explanation* ex, unsigned basic_inf_int_j, const row_strip<mpq>& row, int_solver& lia) :
         m_t(t),
@@ -541,10 +430,10 @@ public:
             if (r != lia_move::cut)
                 continue;
             cut_result cr = {cc.m_dep, lia.m_t, lia.m_k, cc.m_polarity, j};
-            if (abs(cr.polarity) == 1)  // need to delay the update of the bounds for j in a polar case, because simplify_inequality relies on the old bounds
+            if (abs(cr.polarity) == 1)  // delay the update of the bounds for j in a polar case
                 polar_vars.push_back( {j, cr.polarity,  cc.m_dep} ); 
            
-            if (!is_small_cut(lia.m_t)) {
+            if (cc.m_found_big || !is_small_cut(lia.m_t)) {
                 big_cuts.push_back(cr);
                 continue;
             }
